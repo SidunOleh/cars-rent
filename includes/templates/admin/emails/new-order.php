a3bba3b7e903802c8bc4138f5abe2979a62c60ce
Oleh Sidun <sidunoleh@gmail.com>
2023-09-14 00:20:01 +0300

    comment preheader

--- includes/templates/admin/emails/new-order.php
+++ includes/templates/admin/emails/new-order.php
@@ -112,9 +112,9 @@
 <body style="background-color: #e9ecef;">
 
     <!-- start preheader -->
-    <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+    <!-- <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
         <?php echo get_the_title( $order->car_id ) ?>
-    </div>
+    </div> -->
     <!-- end preheader -->
 
     <!-- start body -->
